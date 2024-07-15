cd0b477c8014246b2605579140f5953bbe809307
123dd654 <161431124+123dd654@users.noreply.github.com>
2024-07-15 12:05:05 +0900

    first

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { IoSearch } from "react-icons/io5";
+import { FaRegLightbulb } from "react-icons/fa";
+import { GiQuillInk } from "react-icons/gi";
+
+export default function Header() {
+  return (
+    <header className="bg-[#F9F4E1]">
+      <section className="max-w-7xl mx-auto flex justify-between items-center py-3 px-6 border-b-2">
+        <div className="flex items-center">
+          <GiQuillInk />
+          <h1 className="text-xl font-normal text-blue-950">Coding Diary</h1>
+        </div>
+        <nav className="flex space-x-4">
+          <a href="#" className="text-blue-950">
+            Home
+          </a>
+          <a href="#" className="text-blue-950">
+            Join
+          </a>
+          <a href="#" className="text-blue-950">
+            Login
+          </a>
+          <a href="#" className="text-blue-950">
+            Board
+          </a>
+          <a href="#" className="text-blue-950">
+            Cate
+          </a>
+        </nav>
+        <div className="icon flex gap-3">
+          <IoSearch />
+          <FaRegLightbulb />
+        </div>
+      </section>
+    </header>
+  );
+}
