1a8e03b9b04ff39d3b6d0d4b666dc197cdffb7c3
123dd654 <161431124+123dd654@users.noreply.github.com>
2024-07-16 08:26:57 +0900

    .

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -4,8 +4,8 @@ const SignIn = () => {
   return (
     <div className="min-h-screen bg-white flex flex-col items-center">
       <main className="flex-grow flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
-        <div className="max-w-lg w-screen bg-white p-12 shadow-lg rounded-3xl border-t-4 border-maingreen transform transition duration-500 hover:scale-105 hover:border-greenhover">
-          <h2 className="text-4xl font-extrabold text-gray-600 mb-8">
+        <div className="max-w-lg w-screen bg-white p-12 transform transition duration-500 ">
+          <h2 className="text-4xl font-extrabold text-blue-950 mb-8 text-center">
             Sign In
           </h2>
           <form className="space-y-8">
@@ -20,8 +20,8 @@ const SignIn = () => {
                 type="email"
                 name="email"
                 id="email"
-                placeholder="이메일을 입력해주세요!"
-                className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:greenhover focus:border-greenhover sm:text-sm transition duration-500 hover:border-greenhover"
+                placeholder="이메일을 입력해주세요"
+                className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:greenhover sm:text-sm transition duration-500 hover:border-greenhover"
               />
             </div>
             <div>
@@ -35,14 +35,14 @@ const SignIn = () => {
                 type="password"
                 name="password"
                 id="password"
-                placeholder="비밀번호를 입력해주세요!"
-                className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:greenhover focus:border-greenhover sm:text-sm transition duration-500 hover:border-greenhover"
+                placeholder="비밀번호를 입력해주세요"
+                className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:greenhover sm:text-sm transition duration-500 hover:border-greenhover"
               />
             </div>
             <div>
               <button
                 type="submit"
-                className="w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-maingreen hover:bg-greenhover hover:text-white hover:border-greenhover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:greenhover transition duration-500 transform hover:scale-105"
+                className="w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-gray-700 bg-[#F9F4E1]  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:greenhover transition duration-500 transform"
               >
                 Sign In
               </button>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Signup = () => {
   return (
-    <div className="min-h-screen bg-white flex flex-col items-center">
+    <div className="min-h-screen bg-white flex flex-col items-center ">
       <main className="flex-grow flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
-        <div className="max-w-lg w-screen bg-white p-12 shadow-lg rounded-3xl border-t-4 border-maingreen transform transition duration-500 hover:scale-105 hover:border-greenhover">
-          <h2 className="text-4xl font-extrabold text-gray-600 mb-8">
+        <div className="max-w-lg w-screen bg-white p-12 border-maingreen transform transition duration-500">
+          <h2 className="text-4xl font-extrabold text-gray-600 mb-8 text-center">
             Sign Up
           </h2>
           <form className="space-y-8">
@@ -20,7 +20,7 @@ const Signup = () => {
                 type="text"
                 name="username"
                 id="username"
-                placeholder="이름을 입력해주세요!"
+                placeholder="이름을 입력해주세요"
                 className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:greenhover focus:border-greenhover sm:text-sm transition duration-500 hover:border-greenhover"
               />
             </div>
@@ -35,7 +35,7 @@ const Signup = () => {
                 type="email"
                 name="email"
                 id="email"
-                placeholder="이메일을 입력해주세요!"
+                placeholder="이메일을 입력해주세요"
                 className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:greenhover focus:border-greenhover sm:text-sm transition duration-500 hover:border-greenhover"
               />
             </div>
@@ -50,7 +50,7 @@ const Signup = () => {
                 type="password"
                 name="password"
                 id="password"
-                placeholder="비밀번호를 입력해주세요!"
+                placeholder="비밀번호를 입력해주세요"
                 className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:greenhover focus:border-greenhover sm:text-sm transition duration-500 hover:border-greenhover"
               />
             </div>
@@ -65,14 +65,14 @@ const Signup = () => {
                 type="password"
                 name="password-confirm"
                 id="password-confirm"
-                placeholder="비밀번호를 재입력해주세요!"
+                placeholder="비밀번호를 재입력해주세요"
                 className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:greenhover focus:border-greenhover sm:text-sm transition duration-500 hover:border-greenhover"
               />
             </div>
             <div>
               <button
                 type="submit"
-                className="w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-maingreen hover:bg-white hover:text-black hover:border-greenhover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:greenhover transition duration-500 transform hover:scale-105"
+                className="w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-gray-700 bg-[#F9F4E1] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:greenhover transition duration-500 transform"
               >
                 Sign Up
               </button>
